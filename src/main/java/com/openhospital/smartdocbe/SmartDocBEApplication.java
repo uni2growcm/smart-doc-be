bd582098cd2417386da46d5619693ef8290aa8d9
hribeiro <henrique.ribeiro@intesys.it>
2025-12-15 10:47:01 +0100

    feat(SD-50): initialize Spring project

---
+++ src/main/java/com/openhospital/smartdocbe/SmartDocBEApplication.java
@@ -0,0 +1,13 @@
+package com.openhospital.smartdocbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDocBEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDocBEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openhospital/smartdocbe/SmartDocBEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openhospital.smartdocbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDocBEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
