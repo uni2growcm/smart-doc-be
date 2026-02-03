9d704df6707b528e79c47829b79e9107ecf640eb
Steve Tsala <stevegires@gmail.com>
2026-02-03 16:02:54 +0100

    fix(SD-70): Fix ci

--- src/test/java/org/openhospital/smartdoc/SmartdocApplicationTests.java
+++ src/test/java/org/openhospital/smartdoc/SmartdocApplicationTests.java
@@ -2,9 +2,11 @@ package org.openhospital.smartdoc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmartdocApplicationTests {
 
     @Test
