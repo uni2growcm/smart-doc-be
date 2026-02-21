433178bf2fe9c335fecb2d5169475b0ce04c8949
Tatemsa <tatemsabill@gmail.com>
2026-02-21 10:34:41 +0100

    Remove unused import

--- src/main/java/org/openhospital/smartdoc/config/SecurityConfig.java
+++ src/main/java/org/openhospital/smartdoc/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
